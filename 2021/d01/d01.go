c41ec34d7036109bb6d946984d765ff0356068e3
Simon Mikkelsen <simonbodall@runbox.com>
2021-12-01 23:12:39 +0100

    Solve 2021/01B

--- 2021/d01/d01.go
+++ 2021/d01/d01.go
@@ -11,3 +11,24 @@ func PartA(measurements []int) int {
 
 	return count
 }
+
+func PartB(measurements []int) int {
+	const window int = 3
+
+	var count int
+
+	for i := 0; i < len(measurements)-window; i++ {
+		var a, b int
+
+		for j := i; j < i+window; j++ {
+			a += measurements[j]
+			b += measurements[j+1]
+		}
+
+		if a < b {
+			count++
+		}
+	}
+
+	return count
+}
--- 2021/d01/d01_test.go
+++ 2021/d01/d01_test.go
@@ -28,6 +28,28 @@ func TestPartA(t *testing.T) {
 	}
 }
 
+func TestPartB(t *testing.T) {
+	tests := []struct {
+		name         string
+		measurements []int
+		increases    int
+	}{
+		{"no input", []int{}, 0},
+		{"sample", []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}, 5},
+		{"puzzle input", puzzleInput(), 1346},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			increases := PartB(test.measurements)
+
+			if increases != test.increases {
+				t.Errorf("expected %d, got %d", test.increases, increases)
+			}
+		})
+	}
+}
+
 func puzzleInput() []int {
 	f, err := os.Open("input")
 
